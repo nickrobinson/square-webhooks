81a1f889020b0f1c46244f0e0b835341221ae884
Nicholas Robinson <nrobinson@squareup.com>
2023-05-24 08:04:35 -0400

    initial commit

---
+++ lib/square/webhooks/version.rb
@@ -0,0 +1,5 @@
+module Square
+  module Webhooks
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/square/webhooks_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Square::Webhooks do
+  it "has a version number" do
+    expect(Square::Webhooks::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
